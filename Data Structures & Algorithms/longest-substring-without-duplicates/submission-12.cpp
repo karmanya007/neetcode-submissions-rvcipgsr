f61d4afcab675136ebb2239e8a96dca5aa7095e5
Karmanya Veer Sharma <karmanyasharma5@gmail.com>
2026-08-13 23:47:30 +0530

    Add: longest-substring-without-duplicates - submission-12

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-12.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+
+        int i = 0, maxLen = 0;
+        unordered_map<char, int> freqMap;
+
+        for(int j = 0; j < n; j++){
+            freqMap[s[j]]++;
+
+            while(i < j && freqMap[s[j]] > 1){
+                freqMap[s[i]]--;
+
+                i++;
+            }
+            maxLen = max(maxLen, j - i + 1);
+        }
+
+        return maxLen;
+    }
+};
