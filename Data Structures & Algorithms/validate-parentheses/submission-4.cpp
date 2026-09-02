818aec1c97312151c2e3cd0501dfe2988c1c0bce
Karmanya Veer Sharma <karmanyasharma5@gmail.com>
2026-09-03 03:08:24 +0530

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char, char> mp = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'},
+        };
+
+        for(char &c : s){
+            if(!mp[c]) st.push(c);
+            else{
+                if(st.empty()) return false;
+                if(st.top() != mp[c]) return false;
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
